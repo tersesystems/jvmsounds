cc58ebb1ddf8f30ba5ece0fa99e09a1839aee438
Will Sargent <will.sargent@gmail.com>
2020-07-14 20:57:26 -0700

    Update allocation tone

--- src/main/java/com/tersesystems/alloctone/Main.java
+++ src/main/java/com/tersesystems/alloctone/Main.java
@@ -17,16 +17,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.DoubleSupplier;
-import java.util.function.LongConsumer;
 
 /**
  * A class which plays the memory allocation tone rate as a sine tone through
- * the Java Audio API.
+ * <a href="https://github.com/philburk/jsyn">jSyn</a>.
  * <p>
  * Please see the <a href="https://tersesystems.com/blog/2020/07/09/logging-vs-memory/">Logging vs Memory</a> for more details.
  * <p>
@@ -36,31 +33,37 @@ import java.util.function.LongConsumer;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        AtomicLong allocRate = new AtomicLong(0L);
-        DoubleSupplier supplier = () -> {
-            double v = allocRate.longValue() / 1e6;
-            System.out.println("Allocation rate in MiB/sec = " + v);
-            return v;
-        };
-        SoftSynth synth = new SoftSynth(supplier);
+        // Increase this number to waste more memory
+        int wastage = 100000;
 
+        AllocationTone tone = new AllocationTone();
         try {
+            AtomicLong allocRate = new AtomicLong(0L);
+            DoubleSupplier supplier = () -> {
+                double v = allocRate.longValue() / 1e6;
+                System.out.println("Allocation rate in MiB/sec = " + v);
+                return v;
+            };
+            tone.start(supplier, Duration.ofMillis(50));
+            tone.setVolume(0.1);
+
             MeterThread meterThread = new MeterThread(allocRate::set);
             meterThread.start();
-            createGarbage();
+
+            createGarbage(wastage);
             Thread.sleep(7500 * 1000);
         } finally {
-            synth.stop();
+            tone.stop();
         }
     }
 
     // Create a very inefficient way of writing to a file.
-    private static void createGarbage() {
+    private static void createGarbage(int wastage) {
         Runnable garbageProducingRunnable = () -> {
             try {
                 Path tempFile = Files.createTempFile("tmp", "tmp");
                 while (true) {
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.toFile()),  100000)) {
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.toFile()), wastage)) {
                         long l = System.currentTimeMillis();
                         StringBuffer b = new StringBuffer().append(Math.random()).append("derp").append(l);
                         writer.write(b.toString());
