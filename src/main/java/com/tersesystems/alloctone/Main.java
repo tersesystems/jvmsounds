f86335e66c8e20cba48938fb0c604c961b078a7d
Will Sargent <will.sargent@gmail.com>
2020-07-14 21:31:43 -0700

    Update with gc collection listener

--- src/main/java/com/tersesystems/alloctone/Main.java
+++ src/main/java/com/tersesystems/alloctone/Main.java
@@ -10,14 +10,23 @@
  */
 package com.tersesystems.alloctone;
 
+import com.sun.management.GarbageCollectionNotificationInfo;
 import jvm_alloc_rate_meter.MeterThread;
 
+import javax.management.Notification;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.MemoryUsage;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.DoubleSupplier;
 
@@ -34,7 +43,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         // Increase this number to waste more memory
-        int wastage = 100000;
+        int wastage = 1000;
 
         AllocationTone tone = new AllocationTone();
         try {
@@ -49,6 +58,13 @@ public class Main {
 
             MeterThread meterThread = new MeterThread(allocRate::set);
             meterThread.start();
+
+            List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+            for (GarbageCollectorMXBean gcbean : gcbeans) {
+                NotificationEmitter emitter = (NotificationEmitter) gcbean;
+                NotificationListener listener = new GarbageCollectionListener();
+                emitter.addNotificationListener(listener, null, null);
+            }
 
             createGarbage(wastage);
             Thread.sleep(7500 * 1000);
@@ -79,5 +95,49 @@ public class Main {
         Thread t1 = new Thread(garbageProducingRunnable);
         t1.setDaemon(true);
         t1.start();
+    }
+
+    /**
+     * Print out some GC statistics.  Maybe we can make record scratches and notes on GC events.
+     */
+    private static class GarbageCollectionListener implements NotificationListener {
+        @Override
+        public void handleNotification(Notification notification, Object handback) {
+            if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                //get the information associated with this notification
+                GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                //get all the info and pretty print it
+                long duration = info.getGcInfo().getDuration();
+                String gctype = info.getGcAction();
+                if ("end of minor GC".equals(gctype)) {
+                    gctype = "Young Gen GC";
+                } else if ("end of major GC".equals(gctype)) {
+                    gctype = "Old Gen GC";
+                }
+                System.out.println();
+                System.out.println(gctype + ": - " + info.getGcInfo().getId() + " " + info.getGcName() + " (from " + info.getGcCause() + ") " + duration + " milliseconds; start-end times " + info.getGcInfo().getStartTime() + "-" + info.getGcInfo().getEndTime());
+
+                //Get the information about each memory space, and pretty print it
+                Map<String, MemoryUsage> membefore = info.getGcInfo().getMemoryUsageBeforeGc();
+                Map<String, MemoryUsage> mem = info.getGcInfo().getMemoryUsageAfterGc();
+                for (Map.Entry<String, MemoryUsage> entry : mem.entrySet()) {
+                    String name = entry.getKey();
+                    MemoryUsage memdetail = entry.getValue();
+                    long memInit = memdetail.getInit();
+                    long memCommitted = memdetail.getCommitted();
+                    long memMax = memdetail.getMax();
+                    long memUsed = memdetail.getUsed();
+                    MemoryUsage before = membefore.get(name);
+                    long beforepercent = ((before.getUsed() * 1000L) / before.getCommitted());
+                    long percent = ((memUsed * 1000L) / before.getCommitted()); //>100% when it gets expanded
+
+                    System.out.print(name + (memCommitted == memMax ? "(fully expanded)" : "(still expandable)") + "used: " + (beforepercent / 10) + "." + (beforepercent % 10) + "%->" + (percent / 10) + "." + (percent % 10) + "%(" + ((memUsed / 1048576) + 1) + "MB) / ");
+                }
+                System.out.println();
+                long totalGcDuration = info.getGcInfo().getDuration();
+                long percent = totalGcDuration * 1000L / info.getGcInfo().getEndTime();
+                System.out.println("GC cumulated overhead " + (percent / 10) + "." + (percent % 10) + "%");
+            }
+        }
     }
 }
